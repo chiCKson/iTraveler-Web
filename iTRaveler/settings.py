353b55e74084a97c71b33f8fd2025104894a7e32
Erandra Jayasundara <erandraj@gmail.com>
2021-10-02 17:05:27 +0530

    removed security issues from settings.py

--- iTRaveler/settings.py
+++ iTRaveler/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%82wleyspg2v0a=+t!5jg^ace(08o#q5bj1&ri630i9*z03v0l'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
